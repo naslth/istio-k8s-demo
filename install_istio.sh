1dd6963febdf691b842483ecaf6d71fee81311dd
naslth <letrunghieu10x@gmail.com>
2024-11-29 03:29:43 +0700

    update

--- install_istio.sh
+++ install_istio.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-# helm repo add istio.io https://storage.googleapis.com/istio-prerelease/daily-build/master-latest-daily/charts
-# helm repo list
 
-# install helm
+# set helm repo
+helm repo add istio.io https://storage.googleapis.com/istio-prerelease/daily-build/master-latest-daily/charts
+helm repo list
+
+# install with helm
+kubectl create namespace istio-system
 helm install istio-base istio/base -n istio-system --set meshConfig.outboundTrafficPolicy.mode=ALLOW_ANY --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="true" --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_AUTO_ALLOCATE="true"
-helm install istiod istio/istiod -n istio-system --wait --set meshConfig.outboundTrafficPolicy.mode=ALLOW_ANY --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="true" --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_AUTO_ALLOCATE="true"
+helm install istiod istio/istiod -n istio-system --set meshConfig.outboundTrafficPolicy.mode=ALLOW_ANY --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="true" --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_AUTO_ALLOCATE="true"
 helm install istio-ingressgateway istio/gateway -n istio-system --set meshConfig.outboundTrafficPolicy.mode=ALLOW_ANY --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="true" --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_AUTO_ALLOCATE="true"
+helm install istio-egressgateway istio/gateway -n istio-system --set service.type=ClusterIP --set meshConfig.outboundTrafficPolicy.mode=ALLOW_ANY --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_CAPTURE="true" --set meshConfig.defaultConfig.proxyMetadata.ISTIO_META_DNS_AUTO_ALLOCATE="true"
+# kubectl create -n istio-system secret tls naslth-credential   --key=myddns_cert/naslth.myddns.me.key   --cert=myddns_cert/naslth.myddns.me.crt 
 
 #check istio
 kubectl get svc -n istio-system
 kubectl get pods -n istio-system
-helm list istio
+helm list
 
-kubectl create -n istio-system secret tls naslth-credential   --key=myddns_cert/naslth.myddns.me.key   --cert=myddns_cert/naslth.myddns.me.crt 
+
 
 #apply 3rd party tools
 #kiali
